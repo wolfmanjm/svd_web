3a3c7c3b6fef5f8367140d7c532e320c7e866a16
Jim Morris <morris@wolfman.com>
2026-02-09 11:22:29 +0000

    make enums unique for name and value

--- data/sql/migrations/00003_add_enums.sql
+++ data/sql/migrations/00003_add_enums.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE enumerations (id SERIAL PRIMARY KEY, field_id integer REFERENCES fields NOT NULL, name text NOT NULL, value text NOT NULL, description text, UNIQUE (field_id, name));
+CREATE TABLE enumerations (id SERIAL PRIMARY KEY, field_id integer REFERENCES fields NOT NULL, name text NOT NULL, value text NOT NULL, description text, UNIQUE (field_id, name, value));
 -- +goose StatementEnd
 
 -- +goose Down
